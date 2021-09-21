7e3481cf6e1c05c9998d9c2d16d212770d1f1f7d
German Tellez <gtellez@condorlabs.io>
2021-09-21 07:03:28 -0500

    WIP - Add stories

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,8 +2,26 @@ import React, { Component } from 'react'
 
 import './Home.css'
 import storiePhoto from './assets/profile_picture_german.png'
-
+import { LeftCircleFilled, RightCircleFilled } from '@ant-design/icons'
 export default class Home extends Component {
+  
+  componentDidMount(){
+  }
+
+  handleScrollToLeft(e){
+    const storiesContainer = document.querySelector('.Feed__stories')
+    e.target.addEventListener('click', () => {
+      storiesContainer.scrollLeft += -250
+    })
+  }
+  
+  handleScrollToRight(e){
+    const storiesContainer = document.querySelector('.Feed__stories')
+    e.target.addEventListener('click', () => {
+      storiesContainer.scrollLeft += 250
+    })
+
+  };
   render() {
     return (
       <>
@@ -11,6 +29,9 @@ export default class Home extends Component {
           
           <div className='Feed'>
             <div className="Feed__stories">
+              <div className='Feed__arrow-container-left'>
+                <LeftCircleFilled onClick={this.handleScrollToLeft} className='Feed__arrow' />
+              </div>
               <ul>
                 <li>
                   <figure>
@@ -57,8 +78,45 @@ export default class Home extends Component {
                     <img src={storiePhoto} alt="User" />
                   </figure>
                 </li>
+                <li>
+                  <figure>
+                    <img src={storiePhoto} alt="User" />
+                  </figure>
+                </li>
+                <li>
+                  <figure>
+                    <img src={storiePhoto} alt="User" />
+                  </figure>
+                </li>
+                <li>
+                  <figure>
+                    <img src={storiePhoto} alt="User" />
+                  </figure>
+                </li>
+                <li>
+                  <figure>
+                    <img src={storiePhoto} alt="User" />
+                  </figure>
+                </li>
+                <li>
+                  <figure>
+                    <img src={storiePhoto} alt="User" />
+                  </figure>
+                </li>
+                <li>
+                  <figure>
+                    <img src={storiePhoto} alt="User" />
+                  </figure>
+                </li>
+                <li>
+                  <figure>
+                    <img src={storiePhoto} alt="User" />
+                  </figure>
+                </li>
               </ul>
-
+              <div className='Feed__arrow-container-right'>
+                <RightCircleFilled onClick={this.handleScrollToRight} className='Feed__arrow' />
+              </div>
             </div>
 
             <div className="Feed__posts">
