7d88e2bd74c4b0058a9e5bb1e74200b50b279f77
German Tellez <gtellez@condorlabs.io>
2021-11-10 23:43:26 -0500

    Add Post model

--- api/src/models/posts.js
+++ api/src/models/posts.js
@@ -1,17 +1,38 @@
+const { DataTypes } = require('sequelize')
+const { sequelize } = require('../utils/mysql')
 
-/* const { Schema, model } = require('mongoose')
-
-const postSchema = Schema({
-  username: String,
-  photoUrl: String,
-  description: String,
-  location: String
-}, {
-  timestamps: {
-    createdAt: 'created_at'
+const Post = sequelize.define('posts', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    unique: true
+  },
+  userId: {
+    type: DataTypes.UUID,
+    allowNull: false,
+    unique: true,
+    field: 'user_id'
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  location: {
+    type: DataTypes.STRING,
+    allowNull: true,
+    field: 'location'
+  },
+  imageUrl: {
+    type: DataTypes.STRING,
+    allowNull: true,
+    field: 'image_url'
   }
+}, {
+  tableName: 'posts',
+  timestamps: true,
+  createdAt: 'created_at',
+  updatedAt: 'updated_at'
 })
 
-const Post = model('Post', postSchema)
-
-module.exports = Post */
+module.exports = Post
